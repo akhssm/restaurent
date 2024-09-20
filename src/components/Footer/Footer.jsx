c86ca75333b6265200aa1aa0e01feaf98429d604
Akshay <mandaakshay20@gmail.com>
2024-09-20 15:48:19 +0530

    Created footer file.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Facebook | Twitter | YouTube</p>
+      <p>Address | Contact</p>
+    </footer>
+  );
+};
+
+export default Footer;
