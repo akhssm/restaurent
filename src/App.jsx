4c16bd438270791df3b1b560e157148f7fad34db
Akshay <mandaakshay20@gmail.com>
2024-09-20 16:34:27 +0530

    added restaurent names and food items.

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,18 @@ function App() {
       <Header />
       <h2>Food Section</h2>
       <div className="food-cards">
-        <FoodCard name="Food 1" price="$10" />
-        <FoodCard name="Food 2" price="$15" />
-        <FoodCard name="Food 3" price="$12" />
-        <FoodCard name="Food 4" price="$18" />
+        <FoodCard name="Pizza" price="$10" />
+        <FoodCard name="Veg Roll" price="$15" />
+        <FoodCard name="Chicken Biryani" price="$12" />
+        <FoodCard name="Muttion Biryani" price="$18" />
       </div>
 
       <h2>Restaurant Section</h2>
       <div className="restaurant-cards">
-        <RestaurantCard name="Restaurant 1" rating="4.5" />
-        <RestaurantCard name="Restaurant 2" rating="4.0" />
-        <RestaurantCard name="Restaurant 3" rating="4.7" />
-        <RestaurantCard name="Restaurant 4" rating="4.2" />
+        <RestaurantCard name="Swagath Restaurent" rating="4.5" />
+        <RestaurantCard name="Shahghouse" rating="4.0" />
+        <RestaurantCard name="Bawarchi" rating="4.7" />
+        <RestaurantCard name="Paradise" rating="4.2" />
       </div>
 
       <Footer />
